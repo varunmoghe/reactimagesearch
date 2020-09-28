d9d61da7f89dd0a5ed988041c8119b1c6b3002a0
Varun Moghe <varunmoghe55@gmail.com>
2020-09-28 10:18:29 +0530

    Light dark theme toggle added

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
-
+import './App.css'
 
 class App extends React.Component {
-    state = { images: [] };
+    state = { images: [], theme: 'light' };
     onSearchSubmit = async term => {
         const response = await unsplash.get('/search/photos', {
             params: {
                 query: term,
-                per_page: 20
+                per_page: 30
             }            
         });
         // .then(response => {
@@ -21,14 +21,32 @@ class App extends React.Component {
         console.log(this.state.images)
     }
 
+    changeTheme= () => {
+        if(this.state.theme === "light") {
+            document.querySelector('body').style.backgroundColor = "#242424";
+            document.getElementById('theme-icon').className = "icon lightbulb";
+            document.getElementById('search-input').id = "search-input-dark";
+            this.setState({ theme: 'dark'});
+        } else {
+            document.querySelector('body').style.backgroundColor = "#ffffff";
+            document.getElementById('theme-icon').className = "icon lightbulb outline";
+            document.getElementById('search-input-dark').id = "search-input";
+            this.setState({theme: 'light'});
+        }
+    }
+
     render() {
         return(
-            <div className="ui container" style={{ marginTop: '10px' }}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
+            <div>
+                <div className="ui container">
+                    <SearchBar onSubmit={this.onSearchSubmit} />
+                </div>
                 <ImageList images={this.state.images} />
+                <button className="circular ui icon button massive" id="theme-changer-btn" onClick={this.changeTheme}>
+                    <i className="icon lightbulb outline" id="theme-icon" />
+                </button>
             </div>
         );
     }
 }
-
 export default App;
